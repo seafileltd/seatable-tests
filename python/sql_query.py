317867170e20247a45d5034aadca186869fe4c40
r350178982 <32759763+r350178982@users.noreply.github.com>
2021-09-29 09:32:48 +0800

    Update sql_query.py

--- python/sql_query.py
+++ python/sql_query.py
@@ -8,28 +8,30 @@ base.auth()
 
 
 sql_list = [
-    (
-        "Select * from Table1 limit 1", "List"
-    ),
-    (
-        "Select 文本,sum(数字) from Table1 group by 文本", "Group"
-    )
+    ("Select * from Table1 limit 1", "List"),
+    ("Select 文本,sum(数字) from Table1 group by 文本", "Group")
 ]
 
 for sql, query_type in sql_list:
-    res_list = base.query(sql)
-    count = len(res_list)
-    print("+++SQL:'%s' Results count: %s+++" % (sql, count))
-    if query_type == 'List' and count >= 1:
-        results = res_list[0]
-        str_list = []
-        for column_name, column_value in results.items():
-            str_list.append("%s : %s" % (column_name, column_value))
-        print("\n".join(str_list))
-    elif query_type == 'Group':
-        for index, res in enumerate(res_list, 1):
-            print("No%s---%s" % (index, res))
-    else:
+    try:
+        res_list = base.query(sql)
+        count = len(res_list)
+        print("+++SQL:'%s' Results count: %s+++" % (sql, count))
+        if query_type == 'List' and count >= 1:
+            results = res_list[0]
+            str_list = []
+            for column_name, column_value in results.items():
+                str_list.append("%s : %s" % (column_name, column_value))
+            print("\n".join(str_list))
+        elif query_type == 'Group':
+            for index, res in enumerate(res_list, 1):
+                print("No%s---%s" % (index, res))
+        else:
+            continue
+    except Exception as e:
+        print("sql:%s, query error: %s" % (sql, e))
+        print("\n")
         continue
+
     print("\n")
 
