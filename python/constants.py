65d03032b6ac25f225f30fb3cabaf1d1f2f07678
r350178982 <32759763+r350178982@users.noreply.github.com>
2021-12-29 12:41:02 +0800

    Update constants.py

--- python/constants.py
+++ python/constants.py
@@ -1,3 +1,4 @@
+# local account
 # DTABLE_WEB_SERVICE_URL = 'http://127.0.0.1:8000'
 # DTABLE_SERVER_URL = 'http://127.0.0.1:5000'
 # TEST_USER_EMAIL = '87d485c2281a42adbddb137a1070f395@auth.local'
@@ -7,11 +8,12 @@
 # TEST_TABLE_NAME = 'TableForTest'
 
 
-DTABLE_WEB_SERVICE_URL = 'https://cloud.seatable.cn'
-DTABLE_SERVER_URL = 'https://dtable-server.seatable.cn/'
-TEST_USER_EMAIL = '510d3988d4074cc19255e080c0cc260f@auth.local'
-TEST_USER_PASSWORD = '111'
-TEST_READ_ONLY_USER_EMAIL = '4035b10b67484140a8ae03913ab6a2dd@auth.local'
-TEST_READ_ONLY_USER_PASSWORD = '111'
+# dev account
+DTABLE_WEB_SERVICE_URL = 'https://dev.seatable.cn'
+DTABLE_SERVER_URL = 'https://dtable-server-dev.seatable.cn/'
+TEST_USER_EMAIL = '7b33259c2c3248aeb0347efbb1ab5df5@auth.local'
+TEST_USER_PASSWORD = '12345678'
+TEST_READ_ONLY_USER_EMAIL = '503ddaa01f6e4458a2602267bef5a80f@auth.local'
+TEST_READ_ONLY_USER_PASSWORD = '12345678'
 TEST_TABLE_NAME = 'TableForTest'
 
