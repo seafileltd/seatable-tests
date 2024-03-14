ff17f87d31492a182ef8a71fcde399f89ff704e5
r350178982 <32759763+r350178982@users.noreply.github.com>
2024-03-14 15:51:52 +0800

    add-api-gateway-tests

---
+++ python/api-gateway-test/main.py
@@ -0,0 +1,165 @@
+from seatable_api import Base
+import requests
+SERVER_URL = "https://dev.seatable.cn"
+API_TOKEN = "1e1ef4db90af4fa73025e8b6f1541f15e1fa0216"
+
+TEST_RESULTS_TABLE_NAME = "TestResults"
+TABLE_NAME = 'Table'
+LINK_TABLE_NAME = 'LinkTable'
+
+class APIGatewayTest(object):
+
+    TEST_TYPE = None
+
+
+
+    def __init__(self, base):
+
+        self.base = base
+        self.dtable_uuid = base.dtable_uuid
+        self.headers = base.headers
+
+
+
+    def format_url(self, api_url):
+
+        gateway_url = "%s/api-gateway" % SERVER_URL
+        return "%s/%s" % (gateway_url, api_url.lstrip('/'))
+
+
+class APIGatewayMetaTest(APIGatewayTest):
+
+    # 测试 api-gateway dtable-db中的原生功能
+
+    TEST_TYPE = 'api-gateway-db'
+
+    def __init__(self, base):
+        super(APIGatewayMetaTest, self).__init__(base)
+
+        self.tmp_row = None
+
+    def format_infos(self, test_name, success, other_infos=None):
+        return {
+            "测试功能": test_name,
+            "API类型": self.TEST_TYPE,
+            "Success": "Yes" if success else "No",
+            "详情": "%s" % other_infos
+        }
+
+    def list_rows(self):
+        api_url = self.format_url('/api/v2/dtables/%s/rows' % self.dtable_uuid)
+        params = {
+            'table_name': TABLE_NAME
+        }
+        resp = requests.get(api_url, params=params, headers=self.headers)
+
+        success, detail = False, ''
+        if resp.status_code == 200:
+            success = True
+            self.tmp_row = resp.json().get('rows')[0]
+        else:
+            success = False
+            detail = resp.content
+        return self.format_infos(
+            'list-rows',
+            success,
+            detail
+        )
+
+    def add_rows(self):
+        api_url = self.format_url('/api/v2/dtables/%s/batch-append-rows/' % self.dtable_uuid)
+        data = {
+            'table_name': TABLE_NAME,
+            'rows': [
+                {'名称': 'AA'}
+            ]
+        }
+        resp = requests.post(api_url, json=data, headers=self.headers)
+
+        success, detail = False, ''
+        if resp.status_code == 200:
+            success = True
+        else:
+            success = False
+            detail = resp.content
+        return self.format_infos(
+            'batch-append-rows',
+            success,
+            detail
+        )
+
+
+    def run_workflow(self):
+        # 定义一个工作流程
+        workflows = [
+            self.list_rows,
+            self.add_rows
+        ]
+
+        for func in workflows:
+            row_data = func()
+            self.base.append_row(TEST_RESULTS_TABLE_NAME, row_data)
+
+
+
+
+class APIGatewayProxyTest(APIGatewayTest):
+
+    # 测试 api-gateway 转发 dtable-server 的功能
+
+    TEST_TYPE = 'dtable-server-proxy'
+
+
+    def __init__(self, base):
+        super(APIGatewayProxyTest, self).__init__(base)
+
+    def format_infos(self, test_name, success, other_infos=None):
+        return {
+            "测试功能": test_name,
+            "API类型": self.TEST_TYPE,
+            "Success": "Yes" if success else "No",
+            "详情": "%s" % other_infos
+        }
+
+    def list_columns(self):
+        api_url = self.format_url('/api/v2/dtables/%s/columns' % self.dtable_uuid)
+        params = {
+            'table_name': TABLE_NAME
+        }
+        resp = requests.get(api_url, params=params, headers=self.headers)
+        success, detail = False, ''
+        if resp.status_code == 200:
+            success = True
+        else:
+            success = False
+            detail = resp.content
+        return self.format_infos(
+            'list-columns',
+            success,
+            detail
+        )
+
+
+    def run_workflow(self):
+        # 定义一个测试流程
+        workflows = [
+            self.list_columns
+        ]
+
+        for func in workflows:
+            row_data = func()
+            self.base.append_row(TEST_RESULTS_TABLE_NAME, row_data)
+
+
+
+if __name__ == '__main__':
+
+    base = Base(API_TOKEN, SERVER_URL)
+    base.auth()
+
+    test_db = APIGatewayMetaTest(base)
+    test_proxy = APIGatewayProxyTest(base)
+
+    test_db.run_workflow()
+    test_proxy.run_workflow()
+
